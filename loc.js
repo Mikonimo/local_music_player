358ae2dac796278f751a5d13aeb9f6b7f9269537
Mark Manani <mikonani413@gmail.com>
2024-11-04 16:25:55 +0300

    first commit

---
+++ loc.js
@@ -0,0 +1,152 @@
+const progress = document.getElementById("progress");
+const song = document.getElementById("song");
+const controlIcon = document.getElementById("controlIcon");
+const playPauseButton = document.querySelector(".play-pause-btn");
+const nextButton = document.querySelector(".controls button.forward");
+const prevButton = document.querySelector(".controls button.backward");
+const songName = document.querySelector(".music-player h1");
+const artistName = document.querySelector(".music-player p");
+
+const songs = [
+  {
+    title: "Gleesh place",
+    name: "King Von",
+    source:
+    href="media/King Von   Gleesh Place Welcome to OBlock.mp3"
+  },
+  {
+    title: "Love Story",
+    name: "Indila",
+    source:
+      "media/Indila  Love Story.mp3",
+  },
+  {
+    title: "Back Again",
+    name: "King Von",
+    source:
+      "media/King Von  Back Again Audio feat Lil Durk  Prince Dre.mp3",
+  },
+  {
+    title: "Why He Told",
+    name: "King Von",
+    source:
+      "media/y2mate.com - King Von  Why He Told Official Video.mp3",
+  },
+  {
+    title: "Same As Us",
+    name: "King Von",
+    source:
+      "media/King Von  Same As Us Lyrics.mp3",
+  },
+
+  {
+    title: "3 Am",
+    name: "King Von",
+    source:
+      "media/utomp3.com - King Von  3 AM Official Video.mp3",
+  },
+  {
+    title: "Something Else",
+    name: "King Von",
+    source:
+      "media/utomp3.com - King Von  Something Else Audio.mp3",
+  },
+];
+
+let currentSongIndex = 3;
+
+function updateSongInfo() {
+  songName.textContent = songs[currentSongIndex].title;
+  artistName.textContent = songs[currentSongIndex].name;
+  song.src = songs[currentSongIndex].source;
+
+  song.addEventListener("loadeddata", () => {});
+}
+
+song.addEventListener("timeupdate", () => {
+  if (!song.paused) {
+    progress.value = song.currentTime;
+  }
+});
+
+song.addEventListener("loadedmetadata", () => {
+  progress.max = song.duration;
+  progress.value = song.currentTime;
+});
+
+song.addEventListener("ended", () => {
+  currentSongIndex = (swiper.activeIndex + 1) % songs.length;
+  updateSongInfo();
+  swiper.slideTo(currentSongIndex); 
+  playSong(); 
+});
+
+function pauseSong() {
+  song.pause();
+  controlIcon.classList.remove("fa-pause");
+  controlIcon.classList.add("fa-play");
+}
+
+function playSong() {
+  song.play();
+  controlIcon.classList.add("fa-pause");
+  controlIcon.classList.remove("fa-play");
+}
+
+function playPause() {
+  if (song.paused) {
+    playSong();
+  } else {
+    pauseSong();
+  }
+}
+
+playPauseButton.addEventListener("click", playPause);
+
+progress.addEventListener("input", () => {
+  song.currentTime = progress.value;
+});
+
+progress.addEventListener("change", () => {
+  playSong();
+});
+
+nextButton.addEventListener("click", () => {
+  currentSongIndex = (currentSongIndex + 1) % songs.length;
+  updateSongInfo();
+  playPause();
+});
+
+prevButton.addEventListener("click", () => {
+  currentSongIndex = (currentSongIndex - 1 + songs.length) % songs.length;
+  updateSongInfo();
+  playPause();
+});
+
+updateSongInfo();
+
+var swiper = new Swiper(".swiper", {
+  effect: "coverflow",
+  centeredSlides: true,
+  initialSlide: 3,
+  slidesPerView: "auto",
+  grabCursor: true,
+  spaceBetween: 40,
+  coverflowEffect: {
+    rotate: 25,
+    stretch: 0,
+    depth: 50,
+    modifier: 1,
+    slideShadows: false,
+  },
+  navigation: {
+    nextEl: ".forward",
+    prevEl: ".backward",
+  },
+});
+
+swiper.on("slideChange", () => {
+  currentSongIndex = swiper.activeIndex;
+  updateSongInfo(); 
+  playPause(); 
+});
